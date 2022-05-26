f2db9d9f420544ecf2ce901ba7e1b8b8d294cb1d
Mohammad Al Amin <alaminbamna08@gmail.com>
2022-05-26 22:30:26 +0600

    .env.local file added

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCJVH7Mv7dfwDVxELt6q3aSLcvTilbjvjM",
-  authDomain: "machine-parts.firebaseapp.com",
-  projectId: "machine-parts",
-  storageBucket: "machine-parts.appspot.com",
-  messagingSenderId: "546852327933",
-  appId: "1:546852327933:web:00ad5485d1de84f1e40994"
+  apiKey: process.env.RACT_APP_API_KEY,
+  authDomain: process.env.RACT_APP_AUTH_DOMAIN,
+  projectId: process.env.RACT_APP_PROJECT_ID,
+  storageBucket: process.env.RACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.RACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.RACT_APP_APP_ID,
 };
 
 // Initialize Firebase
