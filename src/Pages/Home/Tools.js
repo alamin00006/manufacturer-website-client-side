28721b75811b16df031f2f90fba50b07eff2ad7f
Mohammad Al Amin <alaminbamna08@gmail.com>
2022-07-08 16:34:58 +0600

    into home page loading spinner added

--- src/Pages/Home/Tools.js
+++ src/Pages/Home/Tools.js
@@ -1,15 +1,25 @@
 import React, { useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../Loading/Loading';
 import SingleTool from './SingleTool';
 
 const Tools = () => {
 
     const [tools, setTools] = useState([]);
     
-    useEffect(() =>{
-        fetch('https://secure-anchorage-88576.herokuapp.com/parts')
-        .then(res => res.json())
-        .then(data =>setTools(data))
-    }, [])
+    // useEffect(() =>{
+    //     fetch('https://secure-anchorage-88576.herokuapp.com/parts')
+    //     .then(res => res.json())
+    //     .then(data =>setTools(data))
+    // }, [])
+
+    const url = 'https://secure-anchorage-88576.herokuapp.com/parts';
+    const {isLoading} = useQuery(['parts'], ()=> fetch(url).then(res=>res.json()).then(data =>setTools(data)))
+    
+    if(isLoading){
+        return <Loading></Loading>
+    }
+
 
     return (
         <div className='mt-10'>
