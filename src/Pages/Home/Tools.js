122cee936d288a292c91136b34de03ca36f240ac
Mohammad Al Amin <alaminbamna08@gmail.com>
2022-07-08 16:46:02 +0600

    somtheing change

--- src/Pages/Home/Tools.js
+++ src/Pages/Home/Tools.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, {useState } from 'react';
 import { useQuery } from 'react-query';
 import Loading from '../Loading/Loading';
 import SingleTool from './SingleTool';
